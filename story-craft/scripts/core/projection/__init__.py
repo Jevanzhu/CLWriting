438d5a960be770794c08b0a61a6a10c9022922ea
Jevanzhu <jevan.jeoops@gmail.com>
2026-05-30 12:46:50 +0000

    WO-04 建立投影基类

---
+++ story-craft/scripts/core/projection/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Projection writer contracts and public exports."""
+
+from __future__ import annotations
+
+from core.projection.base import ProjectionResult, ProjectionWriter
+
+__all__ = [
+    "ProjectionResult",
+    "ProjectionWriter",
+]
